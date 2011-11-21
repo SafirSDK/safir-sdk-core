e7223e00bf6bf24acffe4e35bb7e04f770f1e0a1
Joakim Åsberg <joakim.asberg@consoden.se>
2011-11-21 09:45:21 +0000

    Added VehicleAppJava
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/trunk@54 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

---
+++ examples/vehicleapp/vehicleapp_core.ss/src/VehicleAppJava/com/saabgroup/safir/samples/vehicleappjava/VehicleAppJava.java
@@ -0,0 +1,92 @@
+/******************************************************************************
+*
+* Copyright Saab AB, 2011 (http://www.safirsdk.com)
+*
+*******************************************************************************
+*
+* This file is part of Safir SDK Core.
+*
+* Safir SDK Core is free software: you can redistribute it and/or modify
+* it under the terms of version 3 of the GNU General Public License as
+* published by the Free Software Foundation.
+*
+* Safir SDK Core is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+package com.saabgroup.safir.samples.vehicleappjava;
+
+import com.saabgroup.safir.dob.Connection;
+import com.saabgroup.safir.dob.StopHandler;
+
+public class VehicleAppJava implements StopHandler {
+
+    /**
+     * The main loop.
+     */
+    private IMainLoop mainLoop;
+    
+    /**
+     * Primary DOB connection.
+     */
+    private Connection dobConnection;
+    
+    /**
+     * The DOB dispatcher
+     */
+    private DobDispatcher dobDispatcher;
+    
+    /**
+     *  Object handlers.
+     */
+    EntityHandler entityHandler;
+    ServiceHandler serviceHandler;
+    
+    public VehicleAppJava() {
+
+        dobConnection = new Connection();
+        mainLoop = new MainLoop();
+        dobDispatcher = new DobDispatcher(dobConnection, mainLoop); 
+        
+        entityHandler = new EntityHandler();
+        serviceHandler = new ServiceHandler();
+    }
+    
+    /**
+     * Start this application.
+     */
+    public void Startup()
+    {
+        // Open DOB connection
+        dobDispatcher.open("VehicleAppJava", "0", 0, this);
+        
+        entityHandler.init();
+        serviceHandler.init();
+        MessageSender.getInstance().init();
+        
+        // Start the one and only thread.
+        mainLoop.start();
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        VehicleAppJava app = new VehicleAppJava();
+        app.Startup();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.saabgroup.safir.dob.StopHandler#onStopOrder()
+     */
+    @Override
+    public void onStopOrder() {
+        mainLoop.stop();
+    }
+}
