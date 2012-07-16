4bdebfc80297d73ccdbaec517988ee74b090a960
Lars Hagström <lars.hagstrom@consoden.se>
2012-07-16 19:47:05 +0000

    add patch check for arm spinlock
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/no_ace@273 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

---
+++ build/patch_check/spinlock_gcc_arm/test.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+*
+* Copyright Saab Systems AB, 2012 (http://www.safirsdk.com)
+*
+* Created by: Lars Hagström / lars@foldspace.nu
+*
+*******************************************************************************
+*
+* This file is part of Safir SDK Core.
+*
+* Safir SDK Core is free software: you can redistribute it and/or modify
+* it under the terms of version 3 of the GNU General Public License as
+* published by the Free Software Foundation.
+*
+* Safir SDK Core is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+#include <boost/shared_ptr.hpp>
+
+
+int main()
+{
+    boost::shared_ptr<int> foo;
+    foo.reset(new int);
+    *foo = 10;
+
+    return 0;
+}
+
