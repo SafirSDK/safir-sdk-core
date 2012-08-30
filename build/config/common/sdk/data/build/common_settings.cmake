b32cf98e9aad697c7b88e83bb943a7eefcd26062
Lars Hagström <lars.hagstrom@consoden.se>
2012-08-30 19:14:51 +0000

    better boost finding
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/trunk@382 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- build/config/common/sdk/data/build/common_settings.cmake
+++ build/config/common/sdk/data/build/common_settings.cmake
@@ -88,8 +88,10 @@ set (Boost_ADDITIONAL_VERSIONS "1.40" "1.40.0" "1.41" "1.41.0" "1.42" "1.42.0" "
 
 set(Boost_USE_MULTITHREADED ON)
 
-# Use boost from tower if it exists
-set(BOOST_ROOT ${SAFIR_SDK})
+# Use boost from tower if it exists, unless specifically set to something else
+if (NOT BOOST_ROOT AND $ENV{BOOST_ROOT} STREQUAL "")
+  set(BOOST_ROOT ${SAFIR_SDK})
+endif()
 find_package(Boost)
 if(Boost_FOUND)
    include_directories(${Boost_INCLUDE_DIRS})
