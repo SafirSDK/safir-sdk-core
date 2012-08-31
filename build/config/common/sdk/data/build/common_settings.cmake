551b09848e1df59c18455e74d8184d36cc5b3caf
Lars Hagström <lars.hagstrom@consoden.se>
2012-08-31 08:23:31 +0000

    attempt to make patch_check more lenient towards not finding boost.chrono
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/trunk@384 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- build/config/common/sdk/data/build/common_settings.cmake
+++ build/config/common/sdk/data/build/common_settings.cmake
@@ -87,7 +87,7 @@ endif ()
 set (Boost_ADDITIONAL_VERSIONS "1.40" "1.40.0" "1.41" "1.41.0" "1.42" "1.42.0" "1.43" "1.43.0" "1.44" "1.44.0" "1.45" "1.45.0" "1.46" "1.46.0" "1.47" "1.47.0" "1.48" "1.48.0" "1.49" "1.49.0" "1.50" "1.50.0" "1.51" "1.51.0" "1.52" "1.52.0")
 
 set(Boost_USE_MULTITHREADED ON)
-
+set (Boost_FIND_QUIETLY 1)
 # Use boost from tower if it exists, unless specifically set to something else
 if (NOT BOOST_ROOT AND "$ENV{BOOST_ROOT}" STREQUAL "")
   set(BOOST_ROOT ${SAFIR_SDK})
@@ -106,7 +106,7 @@ else()
         message(FATAL_ERROR "Failed to find a boost installation!")
     endif()
 endif()
-
+set (Boost_FIND_QUIETLY 0)
 #use dynamic linking with boost
 ADD_DEFINITIONS(-DBOOST_ALL_DYN_LINK)
 
