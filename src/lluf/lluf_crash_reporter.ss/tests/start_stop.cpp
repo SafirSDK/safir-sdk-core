39d525c3c77e0812313b6f95b21473b15d03c422
Lars Hagström <lars.hagstrom@consoden.se>
2012-09-10 11:39:59 +0000

    first tests
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/4.3.x@471 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

---
+++ src/lluf/lluf_crash_reporter.ss/tests/start_stop.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+*
+* Copyright Saab Systems AB, 2012 (http://www.safirsdk.com)
+*
+* Created by: Lars Hagström / lars@foldspace.nu
+*
+*******************************************************************************
+*
+* This file is part of Safir SDK Core.
+*
+* Safir SDK Core is free software: you can redistribute it and/or modify
+* it under the terms of version 3 of the GNU General Public License as
+* published by the Free Software Foundation.
+*
+* Safir SDK Core is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+#include <Safir/Utilities/CrashReporter.h>
+
+void callback()
+{
+
+}
+
+int main()
+{
+    try 
+    {
+        Safir::Utilities::CrashReporter::Start();
+        Safir::Utilities::CrashReporter::RegisterCallback(callback);
+        Safir::Utilities::CrashReporter::Stop();
+        return 0;
+    }
+    catch (...)
+    {
+        return 1;
+    }
+}
+
+
