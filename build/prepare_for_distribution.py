b12717476beeccefec3fecc021d305ed9ef8edbe
Lars Hagström <lars.hagstrom@consoden.se>
2012-10-04 08:38:28 +0000

    fix flushing of printouts
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/trunk@612 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- build/prepare_for_distribution.py
+++ build/prepare_for_distribution.py
@@ -24,8 +24,13 @@
 #
 ###############################################################################
 from __future__ import print_function
-import os, shutil, stat, subprocess
+import os, shutil, stat, subprocess, sys
 from optparse import OptionParser
+
+def log(data):
+    print(data)
+    sys.stdout.flush()
+
 parser = OptionParser()
 parser.add_option("--jenkins", action="store_true",dest="jenkins",default=False,
                   help="Set up and use environment variables for a Jenkins automated build.")
@@ -53,16 +58,16 @@ DOCS_DESTINATION = os.path.join(SAFIR_SDK, "docs")
 
 def copy_file(name,destination):
     if not os.path.isfile(name):
-        print("ERROR! " + name + " is not a file!")
+        log("ERROR! " + name + " is not a file!")
     if not os.path.isdir(destination):
-        print("ERROR! " + destination + " is not a directory!")
+        log("ERROR! " + destination + " is not a directory!")
     try:
         shutil.copy2(name, destination)
         os.chmod(os.path.join(destination,os.path.split(name)[-1]),stat.S_IWRITE|stat.S_IREAD)
     except:
         import filecmp
         if not filecmp.cmp(name,os.path.join(destination,os.path.split(name)[-1])):
-            print("Caught exception while copying " + name + " to " + destination + "/. Maybe the destination file or directory is read-only?")
+            log("Caught exception while copying " + name + " to " + destination + "/. Maybe the destination file or directory is read-only?")
 
 def mkdir(newdir):
     """works the way a good mkdir should :)
@@ -118,7 +123,7 @@ def find_dll(names):
                 fn = os.path.join(path,name)
                 if os.path.isfile(fn):
                     return path
-    print("could not find ", names)
+    log("could not find " + str(names))
 
 def copy_dll(name, alternatives = None, Log_Error = True):
     for path in PATH:
@@ -133,7 +138,7 @@ def copy_dll(name, alternatives = None, Log_Error = True):
             if res:
                 return True
     if Log_Error:
-       print("could not find "+ name)
+       log("could not find "+ name)
     return False
 
 def copy_lib(name, alternatives = None, Log_Error = True):
@@ -153,7 +158,7 @@ def copy_lib(name, alternatives = None, Log_Error = True):
             if res:
                 return True
     if Log_Error:
-       print("could not find "+ name)
+       log("could not find "+ name)
     return False
 
 def copy_libs_from_dir(dir):
@@ -170,7 +175,7 @@ def copy_dlls_from_dir(dir):
 
 def copy_header_dir(dir, patterns=None):
     if not os.path.isdir(dir):
-        print("ERROR! " + dir + " is not a directory")
+        log("ERROR! " + dir + " is not a directory")
         return
     dst = os.path.join(HEADER_DESTINATION,os.path.split(dir)[-1])
     copy_tree(dir,dst, include_patterns=patterns)
@@ -178,7 +183,7 @@ def copy_header_dir(dir, patterns=None):
 
 def copy_headers(dir,files):
     if not os.path.isdir(dir):
-        print("ERROR! " + dir + " is not a directory")
+        log("ERROR! " + dir + " is not a directory")
         return
     for file in files:
         copy_file(os.path.join(dir,file),HEADER_DESTINATION)
@@ -186,7 +191,7 @@ def copy_headers(dir,files):
 def copy_docs_dir(dir, targetname, exclude_regex=None):
     import re
     if not os.path.isdir(dir):
-        print("ERROR! " + dir + " is not a directory");
+        log("ERROR! " + dir + " is not a directory");
         return
     dst = os.path.join(DOCS_DESTINATION,targetname)
     copy_tree(dir, dst, exclude_regex=re.compile(exclude_regex))
@@ -197,7 +202,7 @@ def windows():
     
 
     ##############
-    print("Copying boost stuff")
+    log("Copying boost stuff")
     boost_dir = os.environ.get("BOOST_ROOT")
     if boost_dir is None:
         boost_dir = find_dll(("boost_date_time-vc100-mt-1_41.dll",
@@ -215,19 +220,19 @@ def windows():
     copy_header_dir(os.path.join(boost_dir, "boost"))
 
     ############
-    print("Copying the Qt runtime dll")
+    log("Copying the Qt runtime dll")
     copy_dll("QtCore4.dll")
     copy_dll("QtGui4.dll")
 
     ############
-    print("Copying expat stuff")
+    log("Copying expat stuff")
     copy_dll("libexpat.dll")
     copy_lib("libexpat.lib", ("expat.lib",))
     expat_header_dir = os.path.join(find_dll(("libexpat.dll",)),"..","Source","lib")
     copy_headers(expat_header_dir,("expat.h","expat_external.h"))
 
     ############
-    print("Copying Ada stuff (GNAT runtime, xmlada and templates_parser")
+    log("Copying Ada stuff (GNAT runtime, xmlada and templates_parser")
     copy_dll("libgnat-2011.dll")
     copy_dll("libgnarl-2011.dll")
     copy_dll("libgcc_s_dw2-1.dll")
@@ -242,22 +247,22 @@ def linux():
     pass
 
 def common():
-    print("Copying example apps into the sdk/docs directory")
+    log("Copying example apps into the sdk/docs directory")
     #copy example apps to good spot
     copy_docs_dir("examples/vehicleapp/vehicleapp_core.ss", "examples/vehicleapp", ".*\.svn.*")
     copy_docs_dir("examples/vehicledb/vehicledb_core.ss", "examples/vehicledb", ".*\.svn.*")
     copy_docs_dir("examples/vehiclemmi/vehiclemmi_core.ss", "examples/vehiclemmi", ".*\.svn.*")
 
-    print("Running dobmake clean: ------------")
+    log("Running dobmake clean: ------------")
     #do a dobmake clean in dots_generated
     dobmake_path = os.path.join(SAFIR_RUNTIME,"bin","dobmake.py")
     retcode = subprocess.call(["python",dobmake_path, "-b", "--clean"])
     if retcode != 0:
-        print("Failed to do 'dobmake.py -b --clean'")
-    print("-----------------------------------")
+        log("Failed to do 'dobmake.py -b --clean'")
+    log("-----------------------------------")
 
 def main():
-    print("Preparing Safir SDK Core binary for distribution") 
+    log("Preparing Safir SDK Core binary for distribution") 
     if sys.platform.startswith("linux"):
         linux()
     else:
