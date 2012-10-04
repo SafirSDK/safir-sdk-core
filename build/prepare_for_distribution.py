9fd1a2867c0216bbc0af691ec699f314790ff434
Lars Hagström <lars.hagstrom@consoden.se>
2012-10-04 09:07:27 +0000

    expat updates
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/trunk@617 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- build/prepare_for_distribution.py
+++ build/prepare_for_distribution.py
@@ -234,10 +234,17 @@ def windows():
     ############
     log("Copying expat stuff")
     copy_dll("libexpat.dll")
-    copy_lib("libexpat.lib", ("expat.lib",))
-    expat_header_dir = os.path.join(find_dll(("libexpat.dll",)),"..","Source","lib")
-    copy_headers(expat_header_dir,("expat.h","expat_external.h"))
-
+    copy_lib("libexpat.lib")
+    expat_dir=os.path.join(find_dll(("libexpat.dll",)),"..")
+    expat_header_dir_alt1 = os.path.join(expat_dir,"Source","lib")
+    expat_header_dir_alt2 = os.path.join(expat_dir,"include")
+    if os.path.exists(expat_header_dir_alt1):
+        copy_headers(expat_header_dir_alt1,("expat.h","expat_external.h"))
+    elif os.path.exists(expat_header_dir_alt2):
+        copy_headers(expat_header_dir_alt2,("expat.h","expat_external.h"))
+    else:
+        logError("Failed to find expat headers!")
+    
     ############
     log("Copying Ada stuff (GNAT runtime, xmlada and templates_parser")
     copy_dll("libgnat-2011.dll")
