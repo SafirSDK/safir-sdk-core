46785c6fd381cfd503c3dff0c4ad96feadc560e7
Lars Hagström <lars.hagstrom@consoden.se>
2012-10-21 23:32:06 +0000

    make mono resgen retry 100 times, instead of just three, with a 5 second delay between each attempt
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/trunk@720 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- build/config/common/sdk/data/build/mono_resgen.cmake
+++ build/config/common/sdk/data/build/mono_resgen.cmake
@@ -10,34 +10,43 @@ if (NOT $ENV{VERBOSE} STREQUAL "")
   message ("resgen.cmake command line: ${RESGEN_EXECUTABLE} ${resx_file} ${resource_file}")
 endif()
 
-execute_process(
-  COMMAND ${RESGEN_EXECUTABLE} ${resx_file} ${resource_file}
-  RESULT_VARIABLE result
-  OUTPUT_VARIABLE output
-  ERROR_VARIABLE output)
+set(ATTEMPT 100)
+while(ATTEMPT GREATER 0)
+  math (EXPR ATTEMPT "${ATTEMPT} - 1")
 
-if (NOT result EQUAL 0)
-  message ("Mono resgen failed. This sometimes happens spuriously, so I will try to rerun the command twice more, hoping that it will succeed this time.")    
   execute_process(
     COMMAND ${RESGEN_EXECUTABLE} ${resx_file} ${resource_file}
     RESULT_VARIABLE result
     OUTPUT_VARIABLE output
     ERROR_VARIABLE output)
-  
-  if (NOT result EQUAL 0)
-    message ("Mono resgen failed again; one last attempt:")
-    execute_process(
-      COMMAND ${RESGEN_EXECUTABLE} ${resx_file} ${resource_file}
-      RESULT_VARIABLE result
-      OUTPUT_VARIABLE output
-      ERROR_VARIABLE output)
-    
-    if (NOT result EQUAL 0)
-      message ("Mono resgen failed again, this time it is probably a real error:")
-      message(FATAL_ERROR "${output}")
-    endif()
-  endif()
-endif()
 
-#success!
-message("${output}")
+  if (result EQUAL 0)
+    message("${output}")
+    return()
+  endif()
+  message ("Mono resgen failed. This sometimes happens spuriously, so I will try to rerun the command, hoping that it will succeed this time.")
+  #first attempt to sleep for a bit
+  set(delay 5)
+  # use the sleep command if we can find it
+  find_program (SLEEP sleep)
+  if (SLEEP)
+    execute_process (
+      COMMAND "${SLEEP}" ${delay}
+      TIMEOUT ${delay}
+      ERROR_QUIET OUTPUT_QUIET
+      )
+  elseif(WIN32)    #on windows we can work around not finding it by using ping
+    find_program (PING ping)
+    execute_process (
+      COMMAND ${PING} 127.0.0.1 -n ${delay} -w 1000
+      TIMEOUT ${delay}
+      ERROR_QUIET OUTPUT_QUIET)
+  else ()
+    message (WARNING "Cannot delay retries as neither sleep nor ping command is available!")
+  endif ()
+endwhile()
+
+if (NOT result EQUAL 0)
+  message ("Mono resgen failed a lot of times,  so it is probably a real error:")
+  message(FATAL_ERROR "${output}")
+endif()
