010a6232184be8a28eba43ac129b1edc5bcfde92
Anders Widén <anders.widen@consoden.se>
2012-10-30 11:41:15 +0000

    The lock monitor thread now waits for EntityTypes and ServiceTypes to be initialized before starting to access the shared memory structures. Before, depending on the thread scheduling pattern, we could get an Ensure to trigger at start of dose_main. Also, the stop handling of the internal thread has been improved.
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/4.3.x@772 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- src/dose/dose_main.ss/src/dose_main_lock_monitor.cpp
+++ src/dose/dose_main.ss/src/dose_main_lock_monitor.cpp
@@ -66,92 +66,113 @@ namespace Internal
     LockMonitor::~LockMonitor()
     {
         m_checkerThread.interrupt();
+        m_checkerThread.join();
     }
 
     void LockMonitor::Check()
     {
-        // We use the parameter DoseMainThreadWatchdogTimeout divided by 4 to determine how long to wait for a single lock. 
-        const boost::posix_time::seconds lockTimeout(Safir::Dob::NodeParameters::DoseMainThreadWatchdogTimeout() / 4); 
-
-        for (;;)
+        try
         {
+            // We use the parameter DoseMainThreadWatchdogTimeout divided by 4 to determine how long to wait for a single lock. 
+            const boost::posix_time::seconds lockTimeout(Safir::Dob::NodeParameters::DoseMainThreadWatchdogTimeout() / 4);
 
-            boost::this_thread::sleep(boost::get_system_time() + boost::posix_time::seconds(15));
-
-            bool serviceLocksOk = true;
-            for (Dob::Typesystem::TypeIdVector::const_iterator it = m_serviceTypeIds.begin();
-                 it != m_serviceTypeIds.end();
-                 ++it)
+            // Wait for the shared memory to be initialized
+            for (;;)
             {
-                for (ContextId context = 0; context < Safir::Dob::NodeParameters::NumberOfContexts(); ++context)
+                if (Safir::Dob::Internal::ServiceTypes::IsInitialized() &&
+                    Safir::Dob::Internal::EntityTypes::IsInitialized())
                 {
-                    if (!Safir::Dob::Internal::ServiceTypes::Instance().CanAcquireContainerWriterLock(*it, context, lockTimeout))
-                    {
-                        if (m_loggingIsEnabled)
-                        {
-                            std::ostringstream ostr;
-                            ostr << "Have tried " << lockTimeout.total_seconds() <<
-                                " seconds to get an exclusive lock (type lock or registration container) "
-                                "for type " << Safir::Dob::Typesystem::Utilities::ToUtf8(Typesystem::Operations::GetName(*it)) <<
-                                " in context " << context << ". Probably an application has "
-                                "terminated in an improper way leaving a locked lock behind!" << std::endl;
-                            lllerr << ostr.str().c_str();
-                            Safir::Utilities::Internal::PanicLogging::Log(ostr.str());
-                        }
-                        serviceLocksOk = false;
-                    }                    
+                    break;
                 }
+                boost::this_thread::sleep(boost::posix_time::milliseconds(10));
             }
 
-            bool entityLocksOk = true;
-            for (Dob::Typesystem::TypeIdVector::const_iterator it = m_entityTypeIds.begin();
-                 it != m_entityTypeIds.end();
-                 ++it)
+            for (;;)
             {
-                for (ContextId context = 0; context < Safir::Dob::NodeParameters::NumberOfContexts(); ++context)
+
+                boost::this_thread::sleep(boost::posix_time::seconds(15));
+
+                bool serviceLocksOk = true;
+                for (Dob::Typesystem::TypeIdVector::const_iterator it = m_serviceTypeIds.begin();
+                    it != m_serviceTypeIds.end();
+                    ++it)
                 {
-                    if (!Safir::Dob::Internal::EntityTypes::Instance().CanAcquireContainerWriterLock(*it, context, lockTimeout))
+                    for (ContextId context = 0; context < Safir::Dob::NodeParameters::NumberOfContexts(); ++context)
                     {
-                        if (m_loggingIsEnabled)
+                        if (!Safir::Dob::Internal::ServiceTypes::Instance().CanAcquireContainerWriterLock(*it, context, lockTimeout))
                         {
-                            std::ostringstream ostr;
-                            ostr << "Have tried " << lockTimeout.total_seconds() <<
-                                " seconds to get an exclusive lock (type lock, registration container lock or entity container lock) "
-                                "for type " << Safir::Dob::Typesystem::Utilities::ToUtf8(Typesystem::Operations::GetName(*it)) <<
-                                " in context " << context << ". Probably an application has "
-                                "terminated in an improper way leaving a locked lock behind!" << std::endl;
-                            lllerr << ostr.str().c_str();
-                            Safir::Utilities::Internal::PanicLogging::Log(ostr.str());
-                        }
-                        entityLocksOk = false;
+                            if (m_loggingIsEnabled)
+                            {
+                                std::ostringstream ostr;
+                                ostr << "Have tried " << lockTimeout.total_seconds() <<
+                                    " seconds to get an exclusive lock (type lock or registration container) "
+                                    "for type " << Safir::Dob::Typesystem::Utilities::ToUtf8(Typesystem::Operations::GetName(*it)) <<
+                                    " in context " << context << ". Probably an application has "
+                                    "terminated in an improper way leaving a locked lock behind!" << std::endl;
+                                lllerr << ostr.str().c_str();
+                                Safir::Utilities::Internal::PanicLogging::Log(ostr.str());
+                            }
+                            serviceLocksOk = false;
+                        }                    
                     }
                 }
-            } 
 
-            if (!serviceLocksOk || !entityLocksOk)
-            {
-                std::ostringstream ostr;
-                ostr << "Seems one or more shared memory locks are abandoned in a locked state!!" << '\n';
-                if (Safir::Dob::NodeParameters::TerminateDoseMainWhenUnrecoverableError())
+                bool entityLocksOk = true;
+                for (Dob::Typesystem::TypeIdVector::const_iterator it = m_entityTypeIds.begin();
+                    it != m_entityTypeIds.end();
+                    ++it)
                 {
-                    ostr << "Parameter TerminateDoseMainWhenUnrecoverableError is set to true"
-                        " which means that dose_main will now be terminated!!" << std::endl;
-                    lllerr << ostr.str().c_str();
-                    Safir::Utilities::Internal::PanicLogging::Log(ostr.str());
-
-                    boost::this_thread::sleep(boost::get_system_time() + boost::posix_time::seconds(5));
-
-                    abort(); // Terminate dose_main!!!!
-                }
-                else if (m_loggingIsEnabled)
-                {
-                    ostr << "Parameter TerminateDoseMainWhenUnrecoverableError is set to false"
-                        " which means that dose_main will not be terminated!!" << std::endl;
-                    lllerr << ostr.str().c_str();
-                    Safir::Utilities::Internal::PanicLogging::Log(ostr.str());
-                    m_loggingIsEnabled = false;
+                    for (ContextId context = 0; context < Safir::Dob::NodeParameters::NumberOfContexts(); ++context)
+                    {
+                        if (!Safir::Dob::Internal::EntityTypes::Instance().CanAcquireContainerWriterLock(*it, context, lockTimeout))
+                        {
+                            if (m_loggingIsEnabled)
+                            {
+                                std::ostringstream ostr;
+                                ostr << "Have tried " << lockTimeout.total_seconds() <<
+                                    " seconds to get an exclusive lock (type lock, registration container lock or entity container lock) "
+                                    "for type " << Safir::Dob::Typesystem::Utilities::ToUtf8(Typesystem::Operations::GetName(*it)) <<
+                                    " in context " << context << ". Probably an application has "
+                                    "terminated in an improper way leaving a locked lock behind!" << std::endl;
+                                lllerr << ostr.str().c_str();
+                                Safir::Utilities::Internal::PanicLogging::Log(ostr.str());
+                            }
+                            entityLocksOk = false;
+                        }
+                    }
                 } 
+
+                if (!serviceLocksOk || !entityLocksOk)
+                {
+                    std::ostringstream ostr;
+                    ostr << "Seems one or more shared memory locks are abandoned in a locked state!!" << '\n';
+                    if (Safir::Dob::NodeParameters::TerminateDoseMainWhenUnrecoverableError())
+                    {
+                        ostr << "Parameter TerminateDoseMainWhenUnrecoverableError is set to true"
+                            " which means that dose_main will now be terminated!!" << std::endl;
+                        lllerr << ostr.str().c_str();
+                        Safir::Utilities::Internal::PanicLogging::Log(ostr.str());
+
+                        boost::this_thread::sleep(boost::get_system_time() + boost::posix_time::seconds(5));
+
+                        abort(); // Terminate dose_main!!!!
+                    }
+                    else if (m_loggingIsEnabled)
+                    {
+                        ostr << "Parameter TerminateDoseMainWhenUnrecoverableError is set to false"
+                            " which means that dose_main will not be terminated!!" << std::endl;
+                        lllerr << ostr.str().c_str();
+                        Safir::Utilities::Internal::PanicLogging::Log(ostr.str());
+                        m_loggingIsEnabled = false;
+                    } 
+                }
             }
+        }
+        catch (boost::thread_interrupted&)
+        {
+            // Thread was interrupted, which is expected behaviour. By catching this exception we make
+            // sure that the whole program is not aborted, which could otherwise be the case on some platforms
+            // where an unhandled exception in a thread brings down the whole program.
         }
     }
 }
