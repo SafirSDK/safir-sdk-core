b4b7e331adbd012237b15c00982909adfd51da3a
Lars Hagström <lars.hagstrom@consoden.se>
2012-11-01 11:58:23 +0000

    add new test
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/trunk@789 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

---
+++ src/dose/dose_internal.ss/tests/signals_test/test.cpp
@@ -0,0 +1,57 @@
+/******************************************************************************
+*
+* Copyright Saab Systems AB, 2012 (http://www.safirsdk.com)
+*
+* Created by: Lars Hagström / lars.hagstrom@consoden.se
+*
+*******************************************************************************
+*
+* This file is part of Safir SDK Core.
+*
+* Safir SDK Core is free software: you can redistribute it and/or modify
+* it under the terms of version 3 of the GNU General Public License as
+* published by the Free Software Foundation.
+*
+* Safir SDK Core is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+#include "../../src/Signals.h"
+
+using namespace Safir::Dob::Internal;
+
+void user(const ConnectionId& connection)
+{
+    Signals::Instance().GetConnectionSignalWaiter(connection)();
+    Signals::Instance().SignalConnectOrOut();    
+}
+
+int main()
+{
+    std::set<ConnectionId> connids;
+    boost::thread_group threads;
+    for (int i = 0; i < 100; ++i)
+    {
+        ConnectionId id(0,0,i);
+        connids.insert(id);
+        threads.create_thread(boost::bind(user,id));
+    }
+
+    for (std::set<ConnectionId>::iterator it = connids.begin();
+         it != connids.end(); ++it)
+    {
+        Signals::Instance().SignalIn(*it);
+        Signals::Instance().WaitForConnectOrOut();
+    }
+
+    threads.join_all();
+
+    return 0;
+}
+
+
