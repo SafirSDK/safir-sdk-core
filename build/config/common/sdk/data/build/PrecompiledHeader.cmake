c0b94d622d02dc69e98e9dbeb2a1cc7e33c525f6
Lars Hagström <lars.hagstrom@consoden.se>
2013-02-24 13:33:02 +0000

    add copyright and license notice
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/trunk@1277 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- build/config/common/sdk/data/build/PrecompiledHeader.cmake
+++ build/config/common/sdk/data/build/PrecompiledHeader.cmake
@@ -10,6 +10,30 @@
 #
 # GCC: The precompiled header is always automatically included from
 # every header file.
+#
+# Copyright (C) 2009-2013 Lars Christensen <larsch@belunktum.dk>
+# Copyright (C) 2013 Lars Hagström <lars@foldspace.nu>
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the ‘Software’), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED ‘AS IS’, WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 MACRO(ADD_PRECOMPILED_HEADER _targetName _input)
   GET_FILENAME_COMPONENT(_inputWe ${_input} NAME_WE)
   SET(pch_source ${_inputWe}.cpp)
