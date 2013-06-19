d4771e351502a83213821ab043807b86d4584188
Lars Hagström <lars.hagstrom@consoden.se>
2013-06-19 18:39:56 +0000

    update dots_kernel to use typesystem.ini
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/4.3.x_ini@1782 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- src/dots/dots_kernel.ss/src/Safir/Dob/Typesystem/Internal/Kernel.h
+++ src/dots/dots_kernel.ss/src/Safir/Dob/Typesystem/Internal/Kernel.h
@@ -1932,10 +1932,6 @@ extern "C"
 
     DOTS_KERNEL_API void CALLING_CONVENTION DotsC_PeekAtException(DotsC_TypeId & exceptionId);
 
-
-    DOTS_KERNEL_API void CALLING_CONVENTION DotsC_BinaryDump(const char * const blob,
-                                                             const char * const filenamePart);
-
 }
 
 
