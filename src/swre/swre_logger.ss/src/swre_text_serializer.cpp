e73491d4703cadf007b2989c4fef054b777d1153
Anders Widén <anders.widen@consoden.se>
2013-06-27 14:49:43 +0000

    Reverse merged changes to the swre_logger.
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/syslog_aiwi@1855 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- src/swre/swre_logger.ss/src/swre_text_serializer.cpp
+++ src/swre/swre_logger.ss/src/swre_text_serializer.cpp
@@ -21,13 +21,24 @@
 * along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
 *
 ******************************************************************************/
+#include <Safir/SwReports/Internal/FatalErrorReport.h>
+#include <Safir/SwReports/Internal/ErrorReport.h>
+#include <Safir/SwReports/Internal/ResourceReport.h>
+#include <Safir/SwReports/Internal/ProgrammingErrorReport.h>
+#include <Safir/SwReports/Internal/ProgramInfoReport.h>
+
+#include <Safir/Time/TimeProvider.h>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <sstream>
+
 #include "swre_text_serializer.h"
-#include <Safir/SwReports/Internal/ReportSerializer.h>
 
 namespace Safir
 {
 namespace Swre
 {
+
+static const std::wstring nullIndStr = L"*** NOT DEFINED ***";
 
 //-----------------------------------------------------------------------------
 TextSerializer::TextSerializer()
@@ -50,7 +61,72 @@ TextSerializer& TextSerializer::Inst()
 //-----------------------------------------------------------------------------
 std::wstring TextSerializer::SerializeReport(const Safir::SwReports::Internal::ReportPtr report)
 {
-    return Safir::SwReports::Internal::ReportSerializer::SerializeReport(report, m_includeTypeSequenceNumber);
+    std::wstring reportType;
+    std::wstring reportSpecificInfo;
+
+    switch (report->GetTypeId())
+    {
+        case Safir::SwReports::Internal::FatalErrorReport::ClassTypeId:
+        {
+            reportType = L"Fatal Error";
+
+            GetCommonErrorInfo(reportSpecificInfo, boost::static_pointer_cast<Safir::SwReports::Internal::Error>(report));
+        }
+        break;
+
+        case Safir::SwReports::Internal::ErrorReport::ClassTypeId:
+        {
+            reportType = L"Error";
+
+            GetCommonErrorInfo(reportSpecificInfo, boost::static_pointer_cast<Safir::SwReports::Internal::Error>(report));
+        }
+        break;
+
+        case Safir::SwReports::Internal::ResourceReport::ClassTypeId:
+        {
+            const Safir::SwReports::Internal::ResourceReportPtr resourceReport = boost::static_pointer_cast<Safir::SwReports::Internal::ResourceReport>(report);
+
+            reportType = L"Resource";
+
+            reportSpecificInfo = L"Resource Id => ";
+            reportSpecificInfo += resourceReport->Id().IsNull() ? nullIndStr : resourceReport->Id().GetVal();
+            reportSpecificInfo += L'\n';
+
+            std::wostringstream oStr;
+            resourceReport->Allocated().IsNull() ? (oStr << nullIndStr) : (oStr << std::boolalpha << resourceReport->Allocated().GetVal());
+            reportSpecificInfo += L"Resource Allocated => ";
+            reportSpecificInfo += oStr.str();
+            reportSpecificInfo += L'\n';
+        }
+        break;
+
+        case Safir::SwReports::Internal::ProgrammingErrorReport::ClassTypeId:
+        {
+            reportType = L"Programming Error";
+
+            GetCommonErrorInfo(reportSpecificInfo, boost::static_pointer_cast<Safir::SwReports::Internal::Error>(report));
+        }
+        break;
+
+        case Safir::SwReports::Internal::ProgramInfoReport::ClassTypeId:
+        {
+            reportType = L"Program Info";
+        }
+        break;
+
+    }
+
+    std::wstring serRep(L"========== Software Report Received ==========\n"
+                        L"Type => ");
+
+    serRep +=  reportType;
+    serRep += L'\n';
+    GetCommonInfo(serRep, report);
+    serRep += reportSpecificInfo;
+    GetText (serRep, report);
+    serRep += L'\n';
+
+    return serRep;
 }
 
 //-----------------------------------------------------------------------------
@@ -59,5 +135,71 @@ void TextSerializer::SetIncludeTypeSequenceNumber(bool on)
     m_includeTypeSequenceNumber = on;
 }
 
+//-----------------------------------------------------------------------------
+void TextSerializer::GetCommonInfo(std::wstring& str,
+                                   const Safir::SwReports::Internal::ReportPtr report)
+{
+    str += L"Time => ";
+
+    std::wstring s =
+        boost::posix_time::to_iso_extended_wstring
+        (Safir::Time::TimeProvider::ToPtime(report->SourceTimestamp().GetVal()));
+        //(Safir::Time::TimeProvider::GetTime(*report->SourceTimestamp().GetPtr()));
+//     std::wstring s =
+//        boost::posix_time::to_iso_extended_wstring
+//        (report->SourceTimestamp().SetVal(Safir::Time::TimeProvider::GetUtcTime()));
+
+     //report->SourceTimestamp().SetVal(Safir::Time::TimeProvider::GetUtcTime());
+    // Remove microseconds from string (simply truncate the string)
+    // (STAWI: Haven't yet find a way to tell the boost library to produce a string containing
+    //  millisecond resolution)
+    str += s.substr(0, s.size() - 3);
+    str += L'\n';
+
+    str += L"Application connection => ";
+    str += report->ConnectionName().IsNull() ? nullIndStr : report->ConnectionName().GetVal();
+    str += L'\n';
+
+    std::wostringstream connSeqStr;
+    report->ConnectionSequenceNumber().IsNull() ? (connSeqStr << nullIndStr) : (connSeqStr << report->ConnectionSequenceNumber().GetVal());
+    str += L"Sequence number => ";
+    str += connSeqStr.str();
+    str += L'\n';
+
+    if (m_includeTypeSequenceNumber)
+    {
+        std::wostringstream typeSeqStr;
+        report->TypeSequenceNumber().IsNull() ? (typeSeqStr << nullIndStr) : (typeSeqStr << report->TypeSequenceNumber().GetVal());
+        str += L"Report type sequence number => ";
+        str += typeSeqStr.str();
+        str += L'\n';
+    }
+
+    str += L"Node => ";
+    str += report->NodeName().IsNull() ? nullIndStr : report->NodeName().GetVal();
+    str += L'\n';
+}
+
+//-----------------------------------------------------------------------------
+void TextSerializer::GetCommonErrorInfo(std::wstring& str,
+                                        const Safir::SwReports::Internal::ErrorPtr errReport)
+{
+    str += L"Error Code => ";
+    str += errReport->ErrorCode().IsNull() ? nullIndStr : errReport->ErrorCode().GetVal();
+    str += L'\n';
+
+    str += L"Location => ";
+    str += errReport->Location().IsNull() ? nullIndStr : errReport->Location().GetVal();
+    str += L'\n';
+}
+
+//-----------------------------------------------------------------------------
+void TextSerializer::GetText(std::wstring& str,
+                             const Safir::SwReports::Internal::ReportPtr report)
+{
+    str += L"Text =>\n";
+    str += report->Text().IsNull() ? nullIndStr : report->Text().GetVal();
+    str += L'\n';
+}
 }
 }
--- src/swre/swre_logger.ss/src/swre_text_serializer.h
+++ src/swre/swre_logger.ss/src/swre_text_serializer.h
@@ -56,6 +56,15 @@ private:
     TextSerializer();
     TextSerializer(const TextSerializer& rhs);
 
+    void GetCommonInfo(std::wstring& str,
+                       const Safir::SwReports::Internal::ReportPtr report);
+
+    void GetCommonErrorInfo(std::wstring& str,
+                            const Safir::SwReports::Internal::ErrorPtr errReport);
+
+    void GetText(std::wstring& str,
+                 const Safir::SwReports::Internal::ReportPtr errReport);
+
     bool m_includeTypeSequenceNumber;
 
 };
