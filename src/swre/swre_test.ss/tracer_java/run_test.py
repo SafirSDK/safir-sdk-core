1ce7d5bf6cd5e94723a9ec5d074fdaea5666d347
Lars Hagström <lars.hagstrom@consoden.se>
2013-09-08 20:43:07 +0000

    make the test python2 compatible
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/syslog_aiwi@2039 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- src/swre/swre_test.ss/tracer_java/run_test.py
+++ src/swre/swre_test.ss/tracer_java/run_test.py
@@ -58,43 +58,43 @@ def fail(message):
 if stdout_output.count("\n") != 36 or syslog_output.count("\n") != 36:
     fail("lines")
 
-if stdout_output.count("Rymd-B@rje: blahonga") != 6 or syslog_output.count("Rymd-Börje: blahonga") != 6:
+if stdout_output.count(u"Rymd-B@rje: blahonga") != 6 or syslog_output.count(u"Rymd-Börje: blahonga") != 6:
     fail("blahonga")
 
-if stdout_output.count("Rymd-B@rje: blahonga\n") != 3 or syslog_output.count("Rymd-Börje: blahonga\n") != 3:
+if stdout_output.count(u"Rymd-B@rje: blahonga\n") != 3 or syslog_output.count(u"Rymd-Börje: blahonga\n") != 3:
     fail("blahonga newlines")
 
-if stdout_output.count("Razor: brynanuppafj@ssasponken\n") != 3 or syslog_output.count("Razor: brynanuppafjässasponken\n") != 3:
+if stdout_output.count(u"Razor: brynanuppafj@ssasponken\n") != 3 or syslog_output.count(u"Razor: brynanuppafjässasponken\n") != 3:
     fail("brynanuppa")
 
-if stdout_output.count("Rymd-B@rje: blahong@a\n") != 3 or syslog_output.count("Rymd-Börje: blahong®a\n") != 3:
+if stdout_output.count(u"Rymd-B@rje: blahong@a\n") != 3 or syslog_output.count(u"Rymd-Börje: blahong®a\n") != 3:
     fail("blahong®a")
 
-if stdout_output.count("Rymd-B@rje: blahonga@@@\n") != 3 or syslog_output.count("Rymd-Börje: blahongaåäö\n") != 3:
+if stdout_output.count(u"Rymd-B@rje: blahonga@@@\n") != 3 or syslog_output.count(u"Rymd-Börje: blahongaåäö\n") != 3:
     fail("åäö")
 
-if stdout_output.count("Razor: 123.1\n") != 3 or syslog_output.count("Razor: 123.1\n") != 3:
+if stdout_output.count(u"Razor: 123.1\n") != 3 or syslog_output.count(u"Razor: 123.1\n") != 3:
     fail("123.1")
 
-if stdout_output.count("Razor: foobar\n") != 3 or syslog_output.count("Razor: foobar\n") != 3:
+if stdout_output.count(u"Razor: foobar\n") != 3 or syslog_output.count(u"Razor: foobar\n") != 3:
     fail("foobar")
     
-if stdout_output.count("Razor: this is the end\n") != 3 or syslog_output.count("Razor: this is the end\n") != 3:
+if stdout_output.count(u"Razor: this is the end\n") != 3 or syslog_output.count(u"Razor: this is the end\n") != 3:
     fail("this is the end")
 
-if stdout_output.count("Razor: my only friend, the end\n") != 3 or syslog_output.count("Razor: my only friend, the end\n") != 3:
+if stdout_output.count(u"Razor: my only friend, the end\n") != 3 or syslog_output.count(u"Razor: my only friend, the end\n") != 3:
     fail("my only friend, the end")
 
-if stdout_output.count("the end\nRymd-B@rje: of our elaborate plans\n") != 3:
+if stdout_output.count(u"the end\nRymd-B@rje: of our elaborate plans\n") != 3:
     fail("elaborate plans")
 
-if stdout_output.count("crossbones: @\n") != 3 or syslog_output.count("crossbones: \u2620\n") != 3:
+if stdout_output.count(u"crossbones: @\n") != 3 or syslog_output.count(u"crossbones: \u2620\n") != 3:
     fail("crossbones")
 
-if stdout_output.count("interrobang: @\n") != 3 or syslog_output.count("interrobang: \u203d\n") != 3:
+if stdout_output.count(u"interrobang: @\n") != 3 or syslog_output.count(u"interrobang: \u203d\n") != 3:
     fail("interrobang")
 
-if stdout_output.count("@reversed\n") != 3 or syslog_output.count("\u202ereversed\n") != 3:
+if stdout_output.count(u"@reversed\n") != 3 or syslog_output.count(u"\u202ereversed\n") != 3:
     fail("reversed")
 
 print("success")
