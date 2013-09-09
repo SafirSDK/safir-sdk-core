37d6ee78cc27f6e85d1f15212d8c343935b2e009
Anders Widén <anders.widen@consoden.se>
2013-09-09 07:44:28 +0000

    Added Logging Ada tests.
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/syslog_aiwi@2041 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

---
+++ src/logging/logging_test.ss/logging_ada/run_test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright Saab AB, 2013 (http://www.safirsdk.com)
+#
+# Created by: Anders Widén <anders.widen@consoden.se>
+#
+###############################################################################
+#
+# This file is part of Safir SDK Core.
+#
+# Safir SDK Core is free software: you can redistribute it and/or modify
+# it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# Safir SDK Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import print_function
+import subprocess, os, time, sys
+import syslog_server
+
+if sys.platform == "win32":
+    config_type = os.environ.get("CMAKE_CONFIG_TYPE")
+    exe_path = config_type if config_type else ""
+else:
+    exe_path = "."
+    
+sender_path = os.path.join(exe_path, "log_sender")
+
+log_server = syslog_server.SyslogServer()
+
+#Run the program that sends system logs
+proc = subprocess.Popen(sender_path, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
+proc.communicate() # wait until sender program exits
+time.sleep(1)
+
+output = log_server.get_data(1)
+
+if "This is an emergency log" in output:
+##   "This is an alert log" in output and \
+##   "This is a critical log" in output and \
+##   "This is an error log" in output and \
+##   "This is a warning log" in output and \
+##   "This is a notice log" in output and \
+##   "This is an informational log" in output and \
+##   "This is a debug log" in output:   
+    print("Found all expected output!")
+    sys.exit(0)
+else:
+    print("Did not found all expected output!")
+    print(output)
+    sys.exit(1)    
+
+
