5889d69b0d6da815c77865de1e023f5772278855
Lars Hagström <lars.hagstrom@consoden.se>
2013-09-17 18:13:10 +0000

    add java and ada swreport tests
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/syslog_aiwi@2118 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

---
+++ src/swre/swre_test.ss/swreport_java/Sender.java
@@ -0,0 +1,63 @@
+/******************************************************************************
+*
+* Copyright Saab AB, 2013 (http://www.safirsdk.com)
+*
+* Created by: Lars Hagström / lars.hagstrom@foldspace.nu
+*
+*******************************************************************************
+*
+* This file is part of Safir SDK Core.
+*
+* Safir SDK Core is free software: you can redistribute it and/or modify
+* it under the terms of version 3 of the GNU General Public License as
+* published by the Free Software Foundation.
+*
+* Safir SDK Core is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+
+/**
+ * Main test class
+ */
+@SuppressWarnings("deprecation")
+public class Sender {
+    public static void main(String[] args) {
+        com.saabgroup.safir.swreports.SwReport.SendFatalErrorReport("FatalError",
+                                                                    "here",
+                                                                    "Testing SendFatalErrorReport");
+        
+        com.saabgroup.safir.swreports.SwReport.SendErrorReport("Error",
+                                                               "there",
+                                                               "Testing SendErrorReport");
+        
+        com.saabgroup.safir.swreports.SwReport.SendResourceReport("ResourceReport",
+                                                                  false,
+                                                                  "Testing SendResourceReport");
+        
+        com.saabgroup.safir.swreports.SwReport.SendResourceReport("ResourceReport",
+                                                                  true,
+                                                                  "Testing SendResourceReport");
+        
+        com.saabgroup.safir.swreports.SwReport.SendProgrammingErrorReport("ProgrammingError",
+                                                                          "everywhere",
+                                                                          "Testing SendProgrammingErrorReport");
+        
+        com.saabgroup.safir.swreports.SwReport.SendProgramInfoReport("Testing SendProgramInfoReport");
+        
+        
+        com.saabgroup.safir.swreports.SwReport.SendErrorReport("brynanuppafjässasponken", //ä
+                                                               "Don't know\u203d", //interrobang
+                                                               "Testing\nfunny characters");
+        
+
+        com.saabgroup.safir.swreports.SwReport.SendProgramInfoReport("Finished!");
+        
+        System.exit(0);
+    }
+}
