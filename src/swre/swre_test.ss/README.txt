a66cdbd1e53660d3771a73a1755d6486a094d207
Lars Hagström <lars.hagstrom@consoden.se>
2013-09-19 17:44:03 +0000

    add test for tracerbackdoor
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/syslog_aiwi@2151 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- src/swre/swre_test.ss/README.txt
+++ src/swre/swre_test.ss/README.txt
@@ -1,1 +1,3 @@
 The FORCE_LOG functionality is only tested in the tracer_cpp tests, since it is all implemented in the library. No need to test it in the other languages.
+
+The tracer backdoor is also only tested in c++, in the backdoor_tracer tests. Again this is library functionality.
---
+++ src/swre/swre_test.ss/tracer_backdoor/sender.cpp
@@ -0,0 +1,109 @@
+/******************************************************************************
+*
+* Copyright Saab AB, 2011 (http://www.safirsdk.com)
+*
+* Created by: Lars Hagstrom
+*
+*******************************************************************************
+*
+* This file is part of Safir SDK Core.
+*
+* Safir SDK Core is free software: you can redistribute it and/or modify
+* it under the terms of version 3 of the GNU General Public License as
+* published by the Free Software Foundation.
+*
+* Safir SDK Core is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+#include <Safir/Application/Tracer.h>
+#include <Safir/Dob/Connection.h>
+#include <Safir/Dob/NotOpenException.h>
+#include <Safir/Utilities/AsioDispatcher.h>
+#include <boost/bind.hpp>
+#include <boost/lexical_cast.hpp>
+#include <iostream>
+
+class App
+    : public Safir::Dob::StopHandler
+{
+public:
+    App() 
+         : m_dispatcher(m_connection, m_ioService)
+         , m_timer(m_ioService, boost::posix_time::milliseconds(10))
+         , m_razor(L"Razor")
+         , m_rb(L"Rymd-B\u00f6rje") //ö 
+    {}
+        
+        void OnStopOrder() {m_ioService.stop();}
+            
+    void Run()
+    {
+        for (int i = 0; i < 1000; ++i) //allow max 1000 instances
+        {
+            try 
+            {
+                m_connection.Open(L"sender",boost::lexical_cast<std::wstring>(i),0,this,&m_dispatcher);
+                break; //connected, stop trying
+            }
+            catch (const Safir::Dob::NotOpenException&)
+            {
+                //retry connect in loop
+            }
+        }
+
+        Safir::Application::TracerBackdoor::Start(m_connection);
+        m_timer.async_wait(boost::bind(&App::Timeout, this));
+        m_ioService.run();
+
+        Safir::Application::TracerBackdoor::Stop();
+        m_connection.Close();
+    }
+
+    void Timeout()
+    {
+        static int i = 0;
+        m_timer.expires_at(m_timer.expires_at() + boost::posix_time::milliseconds(10));
+        m_timer.async_wait(boost::bind(&App::Timeout, this));
+        m_razor << "foo" << "bar" << 1234 << std::endl;
+        m_rb << "blahonga, " << "blahonga, " << "blahonga" << std::endl;
+        std::wcout << "Have logged " << i << " times." << std::endl;
+
+        ++i;
+    }
+
+private:
+    boost::asio::io_service m_ioService;
+    Safir::Dob::Connection m_connection;
+    Safir::Utilities::AsioDispatcher m_dispatcher;
+
+    boost::asio::deadline_timer m_timer;
+
+    Safir::Application::Tracer m_razor;
+    Safir::Application::Tracer m_rb;
+};
+
+
+int main()
+{
+    try
+    {
+        App app;
+        app.Run();
+        return 0;
+    }
+    catch (...)
+    {
+        std::wcout << "caught exception" << std::endl;
+        return 1;
+    }
+}
+
+
+
+
