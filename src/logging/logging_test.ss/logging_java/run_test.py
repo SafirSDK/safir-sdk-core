5fda344b580c55bc57ffe512eab1ec712de1f56a
Lars Hagström <lars.hagstrom@consoden.se>
2013-09-19 19:08:30 +0000

    add the check_output method to logging tests when we're running python 2.6
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/syslog_aiwi@2156 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- src/logging/logging_test.ss/logging_java/run_test.py
+++ src/logging/logging_test.ss/logging_java/run_test.py
@@ -27,6 +27,23 @@ from __future__ import print_function
 import subprocess, os, time, sys
 import syslog_server
 from safe_print import *
+
+#TODO remove this when we drop python 2.6 support
+if "check_output" not in dir( subprocess ): # duck punch it in!
+    def f(*popenargs, **kwargs):
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise CalledProcessError(retcode, cmd)
+        return output
+    subprocess.check_output = f
+
     
 SAFIR_RUNTIME = os.environ["SAFIR_RUNTIME"]
 
