89ea4f8a7f16728ceb9ced23decee6c266fd30eb
Lars Hagström <lars.hagstrom@consoden.se>
2013-09-19 18:09:45 +0000

    make crash reporter test slightly less likely to fail spuriously
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/syslog_aiwi@2153 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- src/lluf/lluf_crash_reporter.ss/tests/external_signals_test/sleeper.cpp
+++ src/lluf/lluf_crash_reporter.ss/tests/external_signals_test/sleeper.cpp
@@ -34,6 +34,7 @@ int main()
 {
     Safir::Utilities::CrashReporter::RegisterCallback(callback);
     Safir::Utilities::CrashReporter::Start();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(500)); //sleep a little while to let bg thread start.
     std::wcout << "Started" << std::endl;
     boost::this_thread::sleep(boost::posix_time::seconds(60));
     Safir::Utilities::CrashReporter::Stop();
