369528919a07c316dd57b7d1e0d000d401351e17
Lars Hagström <lars.hagstrom@consoden.se>
2014-01-29 08:25:33 +0000

    fix the d suffix for some dlls
    git-svn-id: http://localhost:9111/private/safir_open/safir_sdk_core/branches/gcc_visibility@2725 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- src/lluf/lluf_utils.ss/src/Safir/Utilities/Internal/UtilsExportDefs.h
+++ src/lluf/lluf_utils.ss/src/Safir/Utilities/Internal/UtilsExportDefs.h
@@ -31,7 +31,6 @@
 #else
 #  define LLUF_UTILS_API SAFIR_HELPER_DLL_IMPORT
 #  define SAFIR_LIBRARY_NAME "lluf_utils"
-#  define SAFIR_NO_DEBUG_LIBRARY_SUFFIX
 #  include <Safir/Utilities/Internal/AutoLink.h>
 #endif
 #define LLUF_UTILS_LOCAL SAFIR_HELPER_DLL_LOCAL
