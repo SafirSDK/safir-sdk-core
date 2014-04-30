ea3a37c4ca883fb6a3f71db45f7764375899bf8a
Anders Widén <anders.widen@consoden.se>
2014-04-30 16:12:44 +0200

    Introduced a lock for dose_com transmit queues

--- src/dose/dose_communication.ss/src/dose_com/DoseComTransmit.cpp
+++ src/dose/dose_communication.ss/src/dose_com/DoseComTransmit.cpp
@@ -75,6 +75,9 @@
 #include "../defs/DoseNodeStatus.h"
 #include "../defs/DoseCom_Interface.h"  // to get an error code
 
+#include <ace/Thread_Mutex.h>
+#include <ace/Guard_T.h>
+
 //--------------------
 // Externals
 //--------------------
@@ -108,6 +111,10 @@ extern volatile int * volatile pDbg;
 //-------------------------------------------
 // Local static data
 //-------------------------------------------
+
+// We really don't rely on the lock free approach taken for the original design. This lock is
+// used to protect the different threads from concurrent access to the transmit queues.
+static ACE_Thread_Mutex g_threadLock;
 
 volatile static DOSE_SHARED_DATA_S *g_pShm;
 volatile static NODESTATUS_TABLE *g_pNodeStatusTable;
@@ -353,19 +360,28 @@ static THREAD_API Ack_Thread(void *)
 
         // Check if g_Ack_Queue[] is full. If so do not receive more until
         // there is more space.
-
-        while(
-                ( (g_Ack_Get_ix - g_Ack_Put_ix) == 1)
-                ||
-                ( (g_Ack_Get_ix == 0) && (g_Ack_Put_ix == (MAX_ACK_QUEUE-1)) )
-
-              )
+        for (;;)
         {
-            if(*pDbg>=5)
+            ACE_Guard<ACE_Thread_Mutex> lck(g_threadLock);
+            
+            if(
+                    ( (g_Ack_Get_ix - g_Ack_Put_ix) == 1)
+                    ||
+                    ( (g_Ack_Get_ix == 0) && (g_Ack_Put_ix == (MAX_ACK_QUEUE-1)) )
+                    
+                    )
             {
-                PrintDbg("+++ Ack_Thread(). g_Ack_Queue[] is full, sleeping 30 ms. Ackix=%u Putix=%u\n");
+                if(*pDbg>=5)
+                {
+                    PrintDbg("+++ Ack_Thread(). g_Ack_Queue[] is full, sleeping 30 ms. Ackix=%u Putix=%u\n");
+                }
+                lck.release();
+                DoseOs::Sleep(30);
             }
-            DoseOs::Sleep(30);
+            else
+            {
+                break;
+            }
         }
 
         result = RxSock.RecvFrom2((char *) &UdpMsg, sizeof(UdpMsg), NULL,0);
@@ -387,6 +403,8 @@ static THREAD_API Ack_Thread(void *)
 
         if((UdpMsg.MsgType == MSG_TYPE_ACK) || (UdpMsg.MsgType == MSG_TYPE_NACK))
         {
+            ACE_Guard<ACE_Thread_Mutex> lck(g_threadLock);
+
             if(*pDbg>=5)
             PrintDbg("+++ AckThread() MsgTyp=%s DoseId=%d "
                     "Seq=%d FrNum=%X qIx=%u Put=%u\n",
@@ -1699,6 +1717,7 @@ static THREAD_API TxThread(void *)
             if(*pDbg > 5) PrintDbg("### TxThread() There might be more\n");
             bThereMightBeMore = FALSE;
         }
+
         //===========================================================
         // Scan all,Queues
         // qIx is incremented when there is nothing more on the Queue
@@ -1709,6 +1728,8 @@ static THREAD_API TxThread(void *)
         qIx = 0;
         while(qIx < NUM_TX_QUEUES)
         {
+            ACE_Guard<ACE_Thread_Mutex> lck(g_threadLock);
+
             // There could be the following jobs:
             // 1) Timeout when waiting for Ack
             // 2) The AckThread has received an Ack
@@ -2409,7 +2430,7 @@ Send_The_Message:
                     "#   TxThread[%d] NO Buffer allocated. Ptr is NULL\n",qIx);
 
                 // Improvement: better Recovery
-                DoseOs::Sleep(1000);
+                //DoseOs::Sleep(1000); // Can't have a sleep now that we have introduced g_threadLock.
 
                 // set next GetIxToSend
                 if((UseToSendIx + 1) >= MAX_XMIT_QUEUE) SetIndexToSend(qIx, 0);
@@ -2462,7 +2483,7 @@ Send_The_Message:
                         qIx, TxQ[qIx].PutIx, TxQ[qIx].GetIxToSend,
                         TxQ[qIx].GetIxToAck);
 
-                    DoseOs::Sleep(100);
+                    //DoseOs::Sleep(100); // Can't have a sleep now that we have introduced g_threadLock.
                 }
 
                 result = TxSock.SendTo2(DestIpAddr_nw, DestPort,
@@ -2475,7 +2496,7 @@ Send_The_Message:
                         (char *) &TxMsgHdr, pData, TxMsgHdr.FragmentNumber,
                         TxMsgHdr.SequenceNumber);
 
-                    DoseOs::Sleep(100);
+                    //DoseOs::Sleep(100); // Can't have a sleep now that we have introduced g_threadLock.
                 }
 
                 if(TxMsgHdr.bWantAck)
@@ -2612,6 +2633,8 @@ int CDoseComTransmit::Xmit_Msg(const char *pMsg, dcom_ulong32 MsgLength,
                                dcom_uchar8 PoolDistribution, dcom_uchar8 bUseAck,
                                int Priority, int Destination)
 {
+    ACE_Guard<ACE_Thread_Mutex> lck(g_threadLock);
+
     // Check if we should simulate a stop in the outgoing traffic
     if (g_pShm->InhibitOutgoingTraffic)
     {
