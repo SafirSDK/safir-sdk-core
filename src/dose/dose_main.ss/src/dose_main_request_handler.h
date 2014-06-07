ee3ef90b2ccc09ed8ef2bb101d7b20eed29550fa
Lars Hagström <lars@foldspace.nu>
2014-06-07 21:03:23 +0200

    add dose_main

--- src/dose/dose_main.ss/src/dose_main_request_handler.h
+++ src/dose/dose_main.ss/src/dose_main_request_handler.h
@@ -28,6 +28,7 @@
 #include "dose_main_timers.h"
 #include <Safir/Dob/ErrorResponse.h>
 #include <deque>
+#include <boost/unordered_map.hpp>
 
 namespace Safir
 {
@@ -105,12 +106,12 @@ namespace Internal
 
 
         typedef std::deque<DistributionData> PendingRequests;
-        typedef unordered_map<Identifier, PendingRequests> PendingRequestTable;
+        typedef boost::unordered_map<Identifier, PendingRequests> PendingRequestTable;
 
         PendingRequestTable m_pendingRequests;
 
         Dob::Typesystem::Si64::Second GetTimeout(const Safir::Dob::Typesystem::TypeId typeId) const;
-        typedef unordered_map<Typesystem::TypeId, Typesystem::Si64::Second> TimeoutTable;
+        typedef boost::unordered_map<Typesystem::TypeId, Typesystem::Si64::Second> TimeoutTable;
         mutable TimeoutTable m_timeoutTable;
 
         ExternNodeCommunication* m_ecom;
