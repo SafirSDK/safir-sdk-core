7e1260235a01a457b9f4dff000d5a91c9efa7936
Lars Hagström <lars@foldspace.nu>
2014-07-04 14:30:13 +0200

    make crash reporter not screw up with DESTDIR

--- src/lluf/lluf_crash_reporter.ss/src/google-breakpad/build-breakpad-windows.cmake
+++ src/lluf/lluf_crash_reporter.ss/src/google-breakpad/build-breakpad-windows.cmake
@@ -18,8 +18,3 @@ add_library(breakpad_client STATIC  ${headers} ${sources})
 add_definitions(-DBREAKPAD_NO_TERMINATE_THREAD) 
 add_definitions(-DUNICODE -D_UNICODE)
 add_definitions(/wd4127 /wd4245)
-
-#install in to the source directory so that we're able to find the binary
-#from lluf_crash_reporter
-install(TARGETS breakpad_client
-  ARCHIVE DESTINATION ${CMAKE_CURRENT_SOURCE_DIR})
