6220423cea5f13c597cdad291a6a982b33319228
Lars Hagström <lars@foldspace.nu>
2014-07-14 22:31:04 +0200

    update copyright

--- build/build.py
+++ build/build.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-# Copyright Saab AB, 2009-2013 (http://safir.sourceforge.net)
+# Copyright Saab AB, 2009-2014 (http://safir.sourceforge.net)
 #
 # Created by: Lars Hagstrom / lars.hagstrom@consoden.se
 #
