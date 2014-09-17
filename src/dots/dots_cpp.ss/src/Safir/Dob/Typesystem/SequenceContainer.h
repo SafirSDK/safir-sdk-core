6b923d8dc78fd71a579d2e1b1c325140d37a0879
Joel Ottosson <joel.ottosson@gmail.com>
2014-09-17 07:00:30 +0200

    Added comments to SequenceContainer

--- src/dots/dots_cpp.ss/src/Safir/Dob/Typesystem/SequenceContainer.h
+++ src/dots/dots_cpp.ss/src/Safir/Dob/Typesystem/SequenceContainer.h
@@ -1,8 +1,8 @@
 /******************************************************************************
 *
-* Copyright Saab AB, 2006-2014 (http://safir.sourceforge.net)
+* Copyright Saab AB, 2004-2014 (http://safir.sourceforge.net)
 *
-* Created by: Lars Hagström / stlrha
+* Created by: Joel Ottosson / joot
 *
 *******************************************************************************
 *
@@ -15,7 +15,7 @@
 * Safir SDK Core is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
+* GNU General Public License for more Internals.
 *
 * You should have received a copy of the GNU General Public License
 * along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
@@ -38,6 +38,12 @@ namespace Dob
 {
 namespace Typesystem
 {
+    /**
+     * Container class for sequences of values. A sequence is a collection of values that can dynamically
+     * grow or shrink in size. The whole container can be null, and has a change flag that will automatically
+     * be set when values are added, removed or changed or when the container is set to null. Individual values
+     * does not have change flags and can not be null.
+     */
     template <class T>
     class SequenceContainer : public ContainerBase
     {
@@ -50,7 +56,7 @@ namespace Typesystem
         /**
          * Default Constructor.
          *
-         * Construct a container that is not changed.
+         * Construct a container that is not changed and not null.
          */
         SequenceContainer()
             :m_isNull(true)
@@ -58,10 +64,15 @@ namespace Typesystem
         {
         }
 
-        //implementation of pure virtual in ContainerBase.
+        /**
+         * @brief IsNull - Check if the whole sequence is null.
+         * @return True if the sequence is null, else false.
+         */
         virtual bool IsNull() const {return m_isNull;}
 
-        //implementation of pure virtual in ContainerBase.
+        /**
+         * @brief SetNull - Set the whole sequence to null.
+         */
         virtual void SetNull()
         {
             m_values.clear();
@@ -69,44 +80,79 @@ namespace Typesystem
             m_bIsChanged=true;
         }
 
+        /**
+         * @brief IsChanged - Check if the sequence has changed.
+         * @return True if changed, else false.
+         */
         virtual bool IsChanged() const {return m_bIsChanged;}
 
+        /**
+         * @brief SetChanged - Set the change state of the sequence.
+         * @param changed [in] - If true, the sequence is set to changed, it is set to not changed.
+         */
         virtual void SetChanged(const bool changed) {m_bIsChanged=changed;}
 
+        /**
+         * @brief size - Get the size of the sequence, i.e number of contained values.
+         * @return The number of values in the sequence.
+         */
         size_t size() const {return m_values.size();}
 
+        /**
+         * @brief empty - Check if sequence is empty.
+         * @return True if sequence is empty, else false.
+         */
         bool empty() const {return m_values.empty();}
 
+        /**
+         * @brief front - Get a const reference to the first value in the sequence.
+         * @return Reference to first value.
+         */
         const ContainedType& front() const {return m_values.front();}
 
+        /**
+         * @brief back - Get a const reference to the last value in the sequence.
+         * @return Reference to last value.
+         */
         const ContainedType& back() const {return m_values.back();}
 
+        /**
+         * @brief begin - Get const_iterator pointing to the first element in the sequence.
+         * @return const_iterator.
+         */
         const_iterator begin() const {return m_values.begin();}
 
+        /**
+         * @brief end - Get const_iterator pointing past the last element in the sequence.
+         * @return const_iterator.
+         */
         const_iterator end() const {return m_values.end();}
 
+        /**
+         * @brief clear - Clear the sequence, i.e remove all values. After a call to clear
+         * the sequence will be empty but not automatically set to null.
+         */
         void clear()
         {
             m_bIsChanged=true;
             m_values.clear();
         }
 
+        /**
+         * @brief operator [] - Get const reference to the value with specified index.
+         * @param index [in] - Index of the value to get.
+         * @return Const reference to a value.
+         */
         const ContainedType& operator [](size_t index) const
         {
             return m_values[index];
         }
 
-        void SetVal(size_t index, const ContainedType& val)
-        {
-            m_bIsChanged=true;
-            m_values[index]=val;
-        }
-
-        const ContainedType& GetVal(size_t index) const
-        {
-            return m_values[index];
-        }
-
+        /**
+         * @brief push_back - Insert a new value last in the sequence. If the sequence was null
+         * before it will no longer be null after a call to push_back.
+         * @param val [in] - Value to be inserted.
+         */
         void push_back(const ContainedType& val)
         {
             m_isNull=false;
@@ -114,20 +160,55 @@ namespace Typesystem
             m_values.push_back(val);
         }
 
-        void erase_at(size_t index)
+        /**
+         * @brief SetVal - Update a specific value. Will not add new values.
+         * @param index [in] - Index of the value to set.
+         * @param val [in] - Value to set.
+         */
+        void SetVal(size_t index, const ContainedType& val)
+        {
+            m_bIsChanged=true;
+            m_values[index]=val;
+        }
+
+        /**
+         * @brief GetVal - Get const reference to the value with specified index.
+         * @param index [in] - Index of the value to get.
+         * @return Const reference to a value.
+         */
+        const ContainedType& GetVal(size_t index) const
+        {
+            return m_values[index];
+        }
+
+        /**
+         * @brief InsertAt - Insert a new value at specified index. The sequence size will grow.
+         * @param index [in] - Index of the new value.
+         * @param value [in] - Value to insert.
+         */
+        void InsertAt(size_t index, const ContainedType& value)
+        {
+            m_values.insert(m_values.begin()+index, value);
+        }
+
+        /**
+         * @brief EraseAt - Erase a value at specified index. The sequence will shrink.
+         * @param index [in] - Index of the value to be removed.
+         */
+        void EraseAt(size_t index)
         {
             m_bIsChanged=true;
             m_values.erase(m_values.begin()+index);
         }
 
-        void insert_at(size_t index, const ContainedType& value)
-        {
-            m_values.insert(m_values.begin()+index, value);
-        }
-
+        /**
+         * @brief Copy - Copy all the members from "that" into "this". Types must be the same for this to work!
+         * @param that [in] - The object to copy into this.
+         * @throws SoftwareViolationException If the types are not of the same kind.
+         */
         virtual void Copy(const ContainerBase& /*that*/)
         {
-
+            //TODO
         }
 
     private:
