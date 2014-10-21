b96600e3e887826001e5f90295060e936de063a8
Lars Hagström <lars.hagstrom@consoden.se>
2014-10-21 10:08:01 +0200

    fix version

--- src/util/gactool/AssemblyInfo.cs
+++ src/util/gactool/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿/* ****************************************************************************
 *
 * Copyright Saab AB, 2014 (http://safir.sourceforge.net)
-* 
+*
 * Created by: Lars Hagström / lars.hagstrom@consoden.se
 *
 *******************************************************************************
@@ -25,7 +25,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("gactool")]
@@ -37,23 +37,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("78974B09-89D5-43F1-A09F-40AD4B46E8FD")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//Note: Version information is set by CMake build system
