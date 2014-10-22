3a9decb6a24db661d94e9284f4e9a04d62d81fa1
Lars Hagström <lars.hagstrom@consoden.se>
2014-10-22 14:52:57 +0200

    cleanup

--- src/lluf/lluf_utils.ss/src/ProcessMonitorLinux.h
+++ src/lluf/lluf_utils.ss/src/ProcessMonitorLinux.h
@@ -31,19 +31,10 @@
 #include <boost/function.hpp>
 #include <set>
 
-
-
 namespace Safir
 {
 namespace Utilities
 {
-#if 0
-    // Event struct size
-    const int INOTIFY_EVENT_SIZE = sizeof(struct inotify_event);
-
-    // Event buffer length
-    const int INOTIFY_BUFLEN  = 1024 * (INOTIFY_EVENT_SIZE + 16);
-#endif
     class ProcessMonitorImpl
     {
     public:
@@ -52,20 +43,7 @@ namespace Utilities
                            const boost::chrono::steady_clock::duration& pollPeriod);
 
         void Stop();
-#if 0
-        ~ProcessMonitorImpl();
 
-        //StartMonitorPid and StopMonitorPid induces the io_service to call this
-        //to change the monitored pids on the io_service thread.
-        void ChangeMonitoredPids();
-
-        void HandleInotifyEvent (const boost::system::error_code ec);
-        void HandleTimeout();
-
-        void StartThread();
-        void StopThread();
-
-#endif
         void StartMonitorPid(const pid_t pid)
         {
             m_strand.dispatch(boost::bind(&ProcessMonitorImpl::StartMonitorPidInternal,this,pid));
@@ -75,9 +53,7 @@ namespace Utilities
         {
             m_strand.dispatch(boost::bind(&ProcessMonitorImpl::StopMonitorPidInternal,this,pid));
         }
-#if 0
-        void Run(); // Thread loop
-#endif
+
     private:
         void StartMonitorPidInternal(const pid_t pid);
         void StopMonitorPidInternal(const pid_t pid);
@@ -87,54 +63,13 @@ namespace Utilities
         // Client callback
         boost::function<void(const pid_t pid)> m_callback;
 
-        //The io_service we use to listen to inotify events and to schedule timers
         boost::asio::io_service& m_ioService;
 
         boost::asio::io_service::strand m_strand;
 
         const boost::chrono::steady_clock::duration m_pollPeriod;
         boost::asio::steady_timer m_pollTimer;
-#if 0
 
-        //asio object that wraps the inotify file descriptor
-        boost::asio::posix::stream_descriptor m_inotifyStream;
-
-        //Timer used for polling a process after we have received an inotify event
-        // on its /proc/xxxx directory
-        boost::shared_ptr<boost::asio::deadline_timer> m_timer;
-
-        // Buffer for incoming events
-        unsigned char m_buf[INOTIFY_BUFLEN];
-
-        boost::thread m_thread;
-
-        // Queue of pids to add / remove
-        std::list<pid_t> m_startWatchPids;
-        std::list<pid_t> m_stopWatchPids;
-        boost::mutex m_mutex;
-
-        typedef std::list<pid_t> PidList;
-        class WdInfo
-        {
-        public:
-            PidList m_pidList;
-            std::string m_binPath;
-        };
-
-        typedef std::map<pid_t       /*pid*/           , int /*wd*/, std::less<pid_t> > PidMap;
-        typedef std::map<int         /*wd*/            , WdInfo    , std::less<int> >   WdMap;
-        typedef std::map<std::string /*bin path*/      , int /*wd*/, std::less<std::string> > BinPathMap;
-
-        PidMap m_pidMap;
-        WdMap  m_wdMap;
-        BinPathMap m_binpathMap;
-
-        typedef std::set<int /*wd*/> WdSet;
-
-        WdSet m_wdQueue;
-        boost::posix_time::ptime m_checkUntil;
-        bool m_timerStarted;
-#endif
         std::set<pid_t> m_monitoredPids;
     };
 }
