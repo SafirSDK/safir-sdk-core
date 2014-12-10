6f8a24cb256e2098a8f60e5529f70d541795e696
Lars Hagström <lars.hagstrom@consoden.se>
2014-12-10 15:12:22 +0100

    fix indentation

--- build/jenkins_stuff/run_test.py
+++ build/jenkins_stuff/run_test.py
@@ -273,14 +273,14 @@ def build_examples():
             #TODO: ("src/dots/dots_dobmake.ss/tests/separate_dirs/dous_2" : os.path.join(olddir, "inst")),
             #TODO: ("src/dots/dots_dobmake.ss/tests/separate_dirs/dous_3" : os.path.join(olddir, "inst"))
             )
-        """
-        TODO: to get the above to work we need to have our own typesystem.ini, that contains both the
-        installed dou_search_path and our own "inst" dir from above.
-        One way of doing this would be to use safir_show_config to dump the installed ones to a new
-        set of ini files and then append what we need to them. The set the override env variable to
-        point to that directory.
-        Not really sure it is worth the effort. A lot of fiddling...
-        """
+    """
+    TODO: to get the above to work we need to have our own typesystem.ini, that contains both the
+    installed dou_search_path and our own "inst" dir from above.
+    One way of doing this would be to use safir_show_config to dump the installed ones to a new
+    set of ini files and then append what we need to them. The set the override env variable to
+    point to that directory.
+    Not really sure it is worth the effort. A lot of fiddling...
+    """
     for (builddir, installdir) in dirs:
         os.chdir(builddir)
         cmd = [os.path.join(olddir,"build", "build.py"), "--jenkins", "--skip-tests"]
