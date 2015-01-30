29c35ef4aadcdb26391073221d4b15a181010b19
Lars Hagström <lars@foldspace.nu>
2015-01-30 09:06:59 +0100

    set platform and more compiler flags for csharp

--- src/cmake/SafirLoadCSharp.cmake
+++ src/cmake/SafirLoadCSharp.cmake
@@ -9,3 +9,19 @@ if (NOT CSHARP_FOUND)
 else()
   INCLUDE(CSharpMacros)
 endif()
+
+if (MSVC)
+    SET(CSHARP_COMPILER_FLAGS "-warn:4 -nowarn:1607")
+
+    #Get platform and convert it to lowercase (vs2010 has it as X64 and vs2013 express as x64!)
+    string(TOLOWER "$ENV{Platform}" PLATFORM)
+
+    #make sure we set the arch of dotnet assemblies to be the same as the native code we build.
+    if (PLATFORM STREQUAL "x64")
+      SET(CSHARP_COMPILER_FLAGS "${CSHARP_COMPILER_FLAGS} -platform:x64")
+    else()
+      SET(CSHARP_COMPILER_FLAGS "${CSHARP_COMPILER_FLAGS} -platform:x86")
+    endif()
+else()
+    SET(CSHARP_COMPILER_FLAGS "-warn:4 -nowarn:1587")
+endif()
