50312c2063b6a1988b7dc8fb44717b5e4d1e1d9d
Lars Hagström <lars@foldspace.nu>
2015-03-08 20:21:46 +0100

    remove some obsolete code (not yet replaced with new code)

--- build/jenkins_stuff/run_test.py
+++ build/jenkins_stuff/run_test.py
@@ -318,10 +318,13 @@ def run_database_tests():
         olib_result = subprocess.call(["run_olib_tests",])
 
     log("Running Dope tests")
+    args = ("--driver", os.environ["Driver"],
+            "--hostname", "databases",
+            "--database", "SafirDb")
     if sys.platform == "win32":
-        dope_result = subprocess.call(["run_dope_odbc_backend_test.py",], shell = True)
+        dope_result = subprocess.call(["run_dope_odbc_backend_test.py",] + args, shell = True)
     else:
-        dope_result = subprocess.call(["run_dope_odbc_backend_test",])
+        dope_result = subprocess.call(["run_dope_odbc_backend_test",] + args)
 
     log("Olib tests result:", olib_result)
     log("Dope tests result:", dope_result)
