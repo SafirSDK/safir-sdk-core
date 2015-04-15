7e9ee584f177726bce84b4873ef8f399dd826e1c
Lars Hagström <lars@foldspace.nu>
2015-04-15 09:08:04 +0200

    remove timer unit test, since timers will be removed

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -5,7 +5,6 @@ if ($ENV{SAFIR_SKIP_SLOW_TESTS})
     ElectionHandler_test
     ElectionHandler_test_with_overflows
     LamportClocks
-    dose_main_timers
     dope_file_backend_test
     )
 endif()
