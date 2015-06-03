c8411fbb9d7ad0c8ed713fa25b9e639f3958488d
Lars Hagström <lars@foldspace.nu>
2015-06-03 11:43:14 +0200

    disable multicast in tests

--- src/distribution/system_picture.ss/tests/component_tests/sp_test_dm.cpp
+++ src/distribution/system_picture.ss/tests/component_tests/sp_test_dm.cpp
@@ -371,8 +371,8 @@ int main(int argc, char * argv[])
 
     commNodeTypes.push_back({1,
                 "NodeTypeA",
-                "224.11.22.33:10000", //multicast
-                "224.11.22.44:11000", //multicast
+                "", //no multicast
+                "", //no multicast
                 1000,
                 20,
                 15});
