3e9a8bf47e7ed5ed9919bd6a3c3780a8e0c2a64a
Lars Hagström <lars@foldspace.nu>
2017-06-04 13:56:05 +0200

    add test that restarts a bunch of nodes at the same time

---
+++ src/tests/restart_nodes/Owner.cpp
@@ -0,0 +1,172 @@
+/******************************************************************************
+*
+* Copyright Saab AB, 2017 (http://safir.sourceforge.net)
+*
+* Created by: Lars Hagström / lars.hagstrom@consoden.se
+*
+*******************************************************************************
+*
+* This file is part of Safir SDK Core.
+*
+* Safir SDK Core is free software: you can redistribute it and/or modify
+* it under the terms of version 3 of the GNU General Public License as
+* published by the Free Software Foundation.
+*
+* Safir SDK Core is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+#include <iostream>
+// #include <Safir/Dob/Typesystem/Operations.h>
+// #include <Safir/Dob/Typesystem/Serialization.h>
+#include <Safir/Dob/Connection.h>
+// #include <Safir/Dob/Consumer.h>
+#include <Safir/Utilities/AsioDispatcher.h>
+#include <DoseTest/SynchronousPermanentEntity.h>
+#include <boost/lexical_cast.hpp>
+// #include <boost/algorithm/hex.hpp>
+
+
+class StopHandler :
+    public Safir::Dob::StopHandler
+{
+public:
+    explicit StopHandler(boost::asio::io_service& ioService)
+        : m_ioService(ioService) {}
+    virtual void OnStopOrder() {m_ioService.stop();}
+private:
+    boost::asio::io_service& m_ioService;
+
+};
+
+class EntityOwner
+    : public Safir::Dob::EntityHandlerInjection
+{
+public:
+    explicit EntityOwner(boost::asio::io_service& ioService)
+        : m_ioService(ioService)
+        , m_timer(ioService)
+    {
+        using namespace Safir::Dob;
+        m_connection.Attach();
+
+        m_connection.RegisterEntityHandlerInjection(DoseTest::SynchronousPermanentEntity::ClassTypeId,
+                                                    m_handler,
+                                                    InstanceIdPolicy::HandlerDecidesInstanceId,
+                                                    this);
+
+        for (int i = 0; i < 20; ++i)
+        {
+            auto ent = DoseTest::SynchronousPermanentEntity::Create();
+            ent->Info() = L"test" + boost::lexical_cast<std::wstring>(Typesystem::InstanceId::GenerateRandom().GetRawValue());
+            m_instances.push_back(Typesystem::InstanceId::GenerateRandom());
+            m_connection.SetChanges(ent,
+                                    m_instances.back(),
+                                    m_handler);
+        }
+
+        m_timer.expires_from_now(boost::posix_time::seconds(1));
+        m_timer.async_wait([=](const boost::system::error_code&){Update();});
+    }
+    
+private:
+    void Update()
+    {
+        //std::wcout << "Updating" << std::endl;
+
+        for (auto && inst : m_instances)
+        {
+            auto ent = DoseTest::SynchronousPermanentEntity::Create();
+            ent->Info() = L"test" + boost::lexical_cast<std::wstring>
+                (Safir::Dob::Typesystem::InstanceId::GenerateRandom().GetRawValue());
+            m_connection.SetChanges(ent,
+                                    inst,
+                                    m_handler);
+        }
+
+        m_timer.expires_from_now(boost::posix_time::milliseconds(10));
+        m_timer.async_wait([=](const boost::system::error_code&){Update();});
+    }
+    virtual void OnRevokedRegistration(const Safir::Dob::Typesystem::TypeId,
+        const Safir::Dob::Typesystem::HandlerId&) {}
+
+    virtual void OnCreateRequest(const Safir::Dob::EntityRequestProxy /*entityRequestProxy*/,
+        Safir::Dob::ResponseSenderPtr        /*responseSender*/) {}
+
+    virtual void OnUpdateRequest(const Safir::Dob::EntityRequestProxy /*entityRequestProxy*/,
+        Safir::Dob::ResponseSenderPtr        /*responseSender*/) {}
+
+    virtual void OnDeleteRequest(const Safir::Dob::EntityRequestProxy /*entityRequestProxy*/,
+        Safir::Dob::ResponseSenderPtr        /*responseSender*/) {}
+
+    virtual void OnInjectedNewEntity(const Safir::Dob::InjectedEntityProxy /*injectedEntityProxy*/)
+    {
+    }
+
+    virtual void OnInitialInjectionsDone(const Safir::Dob::Typesystem::TypeId,
+                                         const Safir::Dob::Typesystem::HandlerId&)
+    {
+
+    }
+
+    Safir::Dob::SecondaryConnection m_connection;
+    boost::asio::io_service& m_ioService;
+    boost::asio::deadline_timer m_timer;
+    std::vector<Safir::Dob::Typesystem::InstanceId> m_instances;
+    
+    const Safir::Dob::Typesystem::HandlerId m_handler = Safir::Dob::Typesystem::HandlerId
+        (Safir::Dob::Typesystem::InstanceId::GenerateRandom().GetRawValue());
+};
+
+int main()
+{
+    try
+    {
+        const std::wstring nameCommonPart = L"C++";
+        const std::wstring nameInstancePart = Safir::Dob::Typesystem::InstanceId::GenerateRandom().ToString();
+
+        boost::asio::io_service ioService;
+
+        StopHandler stopHandler(ioService);
+
+        Safir::Dob::Connection connection;
+
+        Safir::Utilities::AsioDispatcher dispatcher(connection,ioService);
+
+        connection.Open(nameCommonPart,
+                        nameInstancePart,
+                        0, // Context
+                        &stopHandler,
+                        &dispatcher);
+        //std::wcout << "Creating owner" << std::endl;
+        EntityOwner owner(ioService);
+        //std::wcout << "Starting ioService" << std::endl;
+        boost::asio::io_service::work keepRunning(ioService);
+        ioService.run();
+        //std::wcout << "Leaving ioService" << std::endl;
+
+        connection.Close();
+    }
+    catch(std::exception & e)
+    {
+        std::wcout << "Caught std::exception! Contents of exception is:" << std::endl
+            << e.what()<<std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::wcout << "Caught ... exception!" << std::endl;
+        return 1;
+    }
+
+    std::wcout << "Exiting" << std::endl;
+
+    return 0;
+}
+
+
---
+++ src/tests/restart_nodes/run_test.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright Saab AB, 2011-2013 (http://safirsdkcore.com)
+#
+# Created by: Lars Hagstrom (lars@foldspace.nu)
+#
+###############################################################################
+#
+# This file is part of Safir SDK Core.
+#
+# Safir SDK Core is free software: you can redistribute it and/or modify
+# it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# Safir SDK Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import print_function
+import os, time, sys, argparse, re
+from testenv import TestEnv, TestEnvStopper
+
+def makeaddr(port):
+    return "127.0.0.1:" + str(port)
+
+def launch_node(args, instance):
+    print("Launching node", instance)
+    os.environ["SAFIR_INSTANCE"] = str(instance)
+    os.environ["SAFIR_NODE_NAME"] = "Node_" + str(instance)
+    os.environ["SAFIR_CONTROL_ADDRESS"] = makeaddr(30000 + instance)
+    os.environ["SAFIR_DATA_ADDRESS"] = makeaddr(40000 + instance)
+    if instance == 0:
+        os.environ["SAFIR_SEED_ADDRESS"] = ""
+    else:
+        os.environ["SAFIR_SEED_ADDRESS"] = makeaddr(30000)
+    os.environ["SYSLOG_SERVER_PORT"] = str(20000 + instance)
+    env = TestEnv(args.safir_control,
+                  args.dose_main,
+                  args.dope_main if instance == 0 else None,
+                  args.safir_show_config,
+                  wait_for_persistence = instance == 0)
+
+    if instance == 0:
+        env.launchProcess("WaitingStatesOwner",args.owner)
+        #while True:
+        #    time.sleep(1)
+        #    print(env.Output("WaitingStatesOwner"))
+        #env.launchProcess("dobexplorer", args.dobexplorer)
+        #env.launchProcess("sate", shutil.which("sate")
+    return env
+
+def parse_arguments():
+    parser = argparse.ArgumentParser("test script")
+    parser.add_argument("--owner", required=True)
+    parser.add_argument("--safir-control", required=True)
+    parser.add_argument("--dobexplorer", required=True)
+    parser.add_argument("--dose_main", required=True)
+    parser.add_argument("--dope_main", required=True)
+    parser.add_argument("--safir-show-config", required=True)
+    parser.add_argument("--safir-generated-paths", required=True)
+
+    arguments = parser.parse_args()
+
+    #add all the environment variables. passed on format A=10;B=20
+    for pair in arguments.safir_generated_paths.split(";"):
+        (name,value) = pair.split("=")
+        print("Setting environment variable", name, "to", value)
+        os.environ[name] = value
+
+    return arguments
+
+CLIENTS=10
+
+def main():
+    args = parse_arguments()
+    server = launch_node(args, 0)
+    output = server.Output("safir_control")
+    incarnation = int(re.search(r"Starting system with incarnation id (.*)",output).group(1))
+    print ("This system has incarnation", incarnation)
+
+    with TestEnvStopper(server):
+        env = dict()
+        try:
+            for _ in range(3):
+                for i in range(1,CLIENTS+1):
+                    env[i] = launch_node(args,i)
+
+                for i in range(1,CLIENTS+1):
+                    env[i].WaitForPersistence()
+                    output = env[i].Output("safir_control")
+                    inc = int(re.search(r"Joined system with incarnation id (.*)",output).group(1))
+                    if inc != incarnation:
+                        print ("Joined invalid incarnation!!!!")
+                        return 1
+
+                for i in range(1,CLIENTS+1):
+                    env[i].killprocs()
+                    if not env[i].ReturnCodesOk():
+                        print("Some process failed")
+                        return
+        finally:
+            for i,e in env.items():
+                e.killprocs()
+
+
+    if not server.ReturnCodesOk():
+        print("Some process exited with an unexpected value")
+        return 1
+
+    return 0
+
+sys.exit(main())
