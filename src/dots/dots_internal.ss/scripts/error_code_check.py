2669266bcdfb01c4bff7d79cdbf6055cbf49e2b9
Lars Hagström <lars@foldspace.nu>
2021-11-08 21:53:26 +0100

    Update another two python scripts to be python3 only

--- src/dots/dots_internal.ss/scripts/error_code_check.py
+++ src/dots/dots_internal.ss/scripts/error_code_check.py
@@ -1,6 +1,32 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os, sys, getopt
+###############################################################################
+#
+# Copyright Saab AB, 2014 (http://safirsdkcore.com)
+#
+# Created by: Joel Ottosson (joel.ottosson@consoden.se)
+#
+###############################################################################
+#
+# This file is part of Safir SDK Core.
+#
+# Safir SDK Core is free software: you can redistribute it and/or modify
+# it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# Safir SDK Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import sys
+import getopt
+
 
 err = []
 tests = set([])
