193161ef53cec2c60eaf79bc03e087f58eec7b39
Lars Hagström <lars@foldspace.nu>
2021-11-25 15:01:59 +0100

    Add more tests to be skipped when skipping long-running tests, and make skipping work when building outside source-tree

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -2,10 +2,18 @@ if ($ENV{SAFIR_SKIP_SLOW_TESTS})
   MESSAGE("Warning: ENVIRONMENT VARIABLE SAFIR_SKIP_SLOW_TESTS IS SET! SKIPPING SOME TESTS!")
   set(CTEST_CUSTOM_TESTS_IGNORE
     LowLevelLogger
+    DataSenderTest
+    DiscovererTest
     ElectionHandler_test
     ElectionHandler_test_with_overflows
     LamportClocks
     dope_file_backend_test
+    dope_none_backend_test
     restart_nodes
+    StopHandler_test
+    tracer_backdoor
+    websocket_component_test
+    websocket_stress_test
+    Incarnation_And_Control_Tests
     )
 endif()
