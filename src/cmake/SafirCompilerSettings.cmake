f144d8a21cd9296d31634645552221c17b9ffa9e
Lars Hagström <lars@foldspace.nu>
2022-09-01 12:52:28 +0200

    Attempt to improve the setting of compiler flags, to ensure that generated-libraries get the correct flags

--- src/cmake/SafirCompilerSettings.cmake
+++ src/cmake/SafirCompilerSettings.cmake
@@ -1,65 +1,55 @@
 #This file sets up some of the compiler flags we want for GCC and MSVC.
+cmake_minimum_required(VERSION 3.13)
 
 set(CMAKE_CXX_STANDARD 17)
 
 if (UNIX)
 
-   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-     if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.9")
-       set(stack_protector_option "-fstack-protector")
-     else()
-       set(stack_protector_option "-fstack-protector-strong")
-     endif()
-   endif()
+  add_compile_options($<$<CXX_COMPILER_ID:GNU>:-fstack-protector-strong>)
 
-   #turn on more warnings, set up use of threads, and set symbol visibility to hide as much as possible
-   SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -Winvalid-pch -Wsuggest-override -pthread -fvisibility=hidden -fvisibility-inlines-hidden ${stack_protector_option}")
-   SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Wpedantic -Winvalid-pch -pthread -fvisibility=hidden -Bsymbolic ${stack_protector_option}")
+  #turn on more warnings, set up use of threads, and set symbol visibility to hide as much as possible
+  add_compile_options(-Wall -Wextra -Wpedantic -Winvalid-pch -pthread -fvisibility=hidden)
+  add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-Wsuggest-override>)
+  add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fvisibility-inlines-hidden>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C>:-Bsymbolic>)
 
-   SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--exclude-libs=ALL -Wl,--warn-common -Wl,--fatal-warnings")
-   SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--exclude-libs=ALL -Wl,--warn-common -Wl,--fatal-warnings")
+  add_link_options(-Wl,--exclude-libs=ALL -Wl,--warn-common -Wl,--fatal-warnings -pthread)
 
-   SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -DNDEBUG")
+  #make sure we get the correct posix version
+  add_compile_definitions(_POSIX_C_SOURCE=200809L)
 
-   #make sure we get the correct posix version
-   ADD_DEFINITIONS(-D_POSIX_C_SOURCE=200809L)
-
-   #this is defined by -pthread (above) in most cases, but at least when creating precompiled headers
-   #it is not, so we define it here as well.
-   ADD_DEFINITIONS(-D_REENTRANT)
+  #this is defined by -pthread (above) in most cases, but at least when creating precompiled headers
+  #it is not, so we define it here as well.
+  add_compile_definitions(_REENTRANT)
 endif ()
 
 if (MSVC)
-   ADD_DEFINITIONS(-DNOMINMAX)
-   ADD_DEFINITIONS(-D_CRT_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_DEPRECATE)
-   ADD_DEFINITIONS(-D_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS)
-   ADD_DEFINITIONS(-D_WINSOCK_DEPRECATED_NO_WARNINGS)
-   ADD_DEFINITIONS(-D_UNICODE -DUNICODE)
-   ADD_DEFINITIONS(-D_WIN32_WINNT=0x0501)
-   ADD_DEFINITIONS(-DWIN32_LEAN_AND_MEAN)
-   ADD_DEFINITIONS(/wd4503) #decorated name length exceeded
-   ADD_DEFINITIONS(/wd4512) #assignment operator could not be generated
+   add_compile_definitions(NOMINMAX)
+   add_compile_definitions(_CRT_SECURE_NO_DEPRECATE _SCL_SECURE_NO_DEPRECATE _CRT_NONSTDC_NO_DEPRECATE)
+   add_compile_definitions(_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS)
+   add_compile_definitions(_WINSOCK_DEPRECATED_NO_WARNINGS)
+   add_compile_definitions(_UNICODE UNICODE)
+   add_compile_definitions(_WIN32_WINNT=0x0501)
+   add_compile_definitions(WIN32_LEAN_AND_MEAN)
+
+   add_compile_options(/wd4503) #decorated name length exceeded
+   add_compile_options(/wd4512) #assignment operator could not be generated
 
    #Disable some warnings from Visual Studio 2015, since we're close to deprecating support for that compiler.
    if (MSVC_VERSION EQUAL 1900)
-     ADD_DEFINITIONS(/wd4714) #marked as __forceinline not inlined
-     ADD_DEFINITIONS(/wd4913) #user defined binary operator ',' exists but no overload could convert all operands, default built-in binary operator ',' used
-     ADD_DEFINITIONS(/wd4800) #forcing value to bool 'true' or 'false' (performance warning)
+     add_compile_options(/wd4714) #marked as __forceinline not inlined
+     add_compile_options(/wd4913) #user defined binary operator ',' exists but no overload could convert all operands, default built-in binary operator ',' used
+     add_compile_options(/wd4800) #forcing value to bool 'true' or 'false' (performance warning)
    endif()
 
    # increase warning level
    # Use the highest warning level for visual studio.
-   IF(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
-     STRING(REGEX REPLACE "/W[0-4]" "/W4"
-       CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-   ELSE()
-     SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
-   ENDIF()
+   add_compile_options(/W4)
 
    #generated libraries sometimes get very large, so we need to use bigobj compiler flag
-   set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /bigobj")
+   add_compile_options(/bigobj)
 
-   #enable auto-inlining for RelWithDebInfo builds 
+   #enable auto-inlining for RelWithDebInfo builds
    IF(CMAKE_CXX_FLAGS_RELWITHDEBINFO MATCHES "/Ob[0-4]")
      STRING(REGEX REPLACE "/Ob[0-4]" "/Ob2"
        CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
@@ -67,10 +57,9 @@ if (MSVC)
      SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} /Ob2")
    ENDIF()
 
-
    #Set linker flag /OPT:REF (eliminates functions and/or data that are never referenced)
    #reduces size of executable to approx the same size as in Release mode.
    #Also disable incremental linking to avoid warning.
-   set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO  "${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO} /OPT:REF /INCREMENTAL:NO")
-   set(CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO  "${CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO} /OPT:REF /INCREMENTAL:NO")
+   add_link_options($<$<CONFIG:RelWithDebInfo>:/OPT:REF>)
+   add_link_options($<$<CONFIG:RelWithDebInfo>:/INCREMENTAL:NO>)
 endif ()
